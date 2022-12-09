bed4da4072ffc8c91ca044040c674dc02da61fe1
giogua03 <giorgiogua03@gmail.com>
2022-12-09 17:28:29 +0100

    meta main

--- src/components/PageMain.vue
+++ src/components/PageMain.vue
@@ -1,5 +1,123 @@
 <template>
-  <div></div>
+  <div>
+    <div class="cont1">
+      <div class="image4"><img src="/images/artist-hero-image-03.png" alt="">
+      </div>
+      <div class="image2"><img src="/images/artist-shape-03.png" alt="">
+      </div>
+      <div class="image3"><img src="/images/artist-hero-image-2.jpg" alt="">
+      </div>
+      <div class="image1"><img src="/images/artist-hero-image-01.jpg" alt="">
+      </div>
+      <div class="class-btn">
+        <h1>Hello, Im Martin</h1>
+        <h2>Artist Coaching And Mentoring Might Be for You</h2>
+        <button>Get started today</button>
+      </div>
+      <div class="image5"><img src="/images/artist-shape-01.png" alt="">
+      </div>
+      <div class="image6"><img src="/images/artist-shape-04.png" alt="">
+      </div>
+      <div class="image7"><img src="/images/artist-shape-05.png" alt="">
+      </div>
+      <div class="image8"><img src="/images/artist-hero-image-04.jpg" alt="">
+      </div>
+    </div>
+    <div class="incom">
+      <h1>After Celebrity</h1>
+      <h1>I understand what it takes to create.</h1>
+      <h1>I can help you whit</h1>
+      <div class="container ">
+    <div class="row">
+    <div class="col">
+      <img src="/images/artist-box-image-01.png" alt="">
+      <h2>Online Courses</h2>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur eligendi id facere suscipit nobis culpa. Sapiente, iusto asperiores ratione reiciendis cupiditate dolorum ad, laudantium </p>
+    </div>
+    <div class="col">
+      <img src="/images/artist-box-image-02.png" alt="">
+      <h2>One to One</h2>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi iusto quo quaerat fuga corporis fugiat, hic excepturi eveniet enim, porro, saepe illum tempora laborum aliquid distinctio ipsam </p>
+    </div>
+    <div class="col">
+      <img src="/images/artist-box-image-03.png" alt="">
+      <h2>Anywhere</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo laudantium ex possimus id doloribus a? Maxime pariatur suscipit, a eaque consectetur blanditiis adipisci nobis dignissimos, quasi </p>
+    </div>
+    <div class="col">
+      <img src="/images/artist-box-image-04.png" alt="">
+      <h2>On Time</h2>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos possimus, magni nulla ipsum cum veritatis excepturi dolore quo illum earum odio delectus voluptatum quaerat assumenda. Doloribus quasi </p>
+    </div>
+    </div>
+    </div>
+    </div>
+    <div class="mentor">
+      <div class="part1">
+        <img src="/images/artist-quote-icon.png" alt="">
+        <h1>I Coach And Mentor Visual Artist, Like You, To Create Like You  Men It</h1>
+        <h1>Martin Garrix</h1>
+      </div>
+      <div class="prt2">
+        <p>   dolor nobis quidem sapiente impedit similique temporibus a ad ipsum asperiores maiores eligendi distinctio iure vel sed animi. amet consectetur adipisicing elit. Aperiam temporibus veritatis hic architecto voluptas. Quae minus eos fugiat earum molestias aspernatur explicabo! Id rerum quaerat reiciendis dolores natus. Amet, provident.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et vel officia eos, aliquid porro autem nam </p>
+        <button>Get Started Today</button>
+      </div>
+    </div>
+    <div class="image-con">
+      <div class="img1"><img src="/images/artist-video-poster.jpg" alt=""></div>
+      <div class="img2"><img src="/images/icon-youtube-play.png" alt=""></div>
+    </div>
+    <div class="cont-4">
+      <h1>Artist Coaching</h1>
+      <h1>Latest Online Courses</h1>
+      <div class="container text-center">
+  <div class="row row-cols-4">
+    <div class="col">
+      <img src="/images/" alt="">
+      <h1></h1>
+      <h2></h2>
+    </div>
+    <div class="col">
+      <img src="/images/" alt="">
+      <h1></h1>
+      <h2></h2>
+    </div>
+    <div class="col">
+      <img src="/images/" alt="">
+      <h1></h1>
+      <h2></h2>
+    </div>
+    <div class="col">
+      <img src="/images/" alt="">
+      <h1></h1>
+      <h2></h2>
+    </div>
+    <div class="col">
+      <img src="/images/" alt="">
+      <h1></h1>
+      <h2></h2>
+    </div>
+    <div class="col">
+      <img src="/images/" alt="">
+      <h1></h1>
+      <h2></h2>
+    </div>
+    <div class="col">
+      <img src="/images/" alt="">
+      <h1></h1>
+      <h2></h2>
+    </div>
+    <div class="col">
+      <img src="/images/" alt="">
+      <h1></h1>
+      <h2></h2>
+    </div>
+  </div>
+</div>
+    </div>
+    
+  </div>
 </template>
 
 <script>
@@ -9,5 +127,122 @@ export default {
 </script>
 
 <style>
-
+.cont1{
+  background-color: #F4F0ef;
+  position: relative;
+  height: 500px;
+}
+.image1 img{
+  position: absolute;
+  left: 6%;
+  bottom: 10%;
+  width: 150px;
+  
+}
+.image2 img{
+  position: absolute;
+  left: 1%;
+  bottom: 7%;
+  width: 250px;
+}
+.image3 img{
+  position: absolute;
+  left: 20%;
+  bottom: 40%;
+}
+.image4 img{
+  position: absolute;
+  left: 18%;
+  bottom: 5%;
+  width: 160px;
+}
+.image5 img{
+  position: absolute;
+  left: 65%;
+  bottom: 2%;
+  width: 510px;
+}
+.image6 img{
+  position: absolute;
+  left: 90%;
+  bottom: 45%;
+  width: 100px;
+}
+.image7 img{
+  position: absolute;
+  left: 80%;
+  bottom: 1%;
+}
+.image8 img{
+  position: absolute;
+  left: 80%;
+  bottom: 5%;
+  width: 230px;
+}
+.class-btn{
+  position: absolute;
+  left: 35%;
+  bottom: 40%;
+  max-width: 500px;
+  text-align: center;
+}
+.class-btn button{
+  color: white;
+  background-color: orange;
+  border-color:orange ;
+}
+button:hover{
+  color: white;
+  background-color: lightblue;
+  border-color:lightblue ;
+}
+.incom h1{
+  text-align: center;
+}
+.incom{
+  margin-top:4rem ;
+  margin-bottom: 2rem;
+}
+.mentor{
+  display: flex;
+  justify-content: center;
+  max-width: 1000px;
+  margin: auto;
+  margin-top: 3rem;
+}
+.part1{
+  text-align: center;
+  max-width: 500px;
+}
+.prt2{
+  max-width: 500px;
+  margin-top: 2.3rem;
+  margin-left: 5rem;
+}
+.prt2 button{
+  background-color: rgb(250, 233, 196);
+  color:orange;
+  border-color: rgb(250, 233, 196);
+}
+.prt2 button:hover{
+  background-color: orange;
+  color: white;
+  border-color:orange;
+}
+.image-con{
+  position: relative;
+  text-align: center;
+  height:  500px;
+  margin: 5rem 0;
+}
+.img1 img{
+  position: absolute;
+  width: 800px;
+  left: 30%;
+}
+.img2 img{
+  position: absolute;
+  top: 42%;
+  left: 49%;
+}
 </style>
